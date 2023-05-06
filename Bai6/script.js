3581602bf741a77af7a72b67d3c50fbef146098a
Khang2612 <phongkhang12392@gmail.com>
2023-05-06 20:52:40 +0700

    My project

---
+++ Bai6/script.js
@@ -0,0 +1,20 @@
+const Pi = 3.14;
+alert(Pi);
+var a = 5;
+console.log(a);
+let b = 9;
+if (b>8){
+    console.log("Giỏi");
+    if (b==10){
+        console.log("Quá Giỏi")
+    }  
+}
+else if(b<8){
+    console.log("Khá");
+}
+else if(b<6.5){
+    console.log("Trung Bình");
+}
+else if(b<5){
+    console.log("Kém");
+}
